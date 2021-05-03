4859c90d0b722a32a790c16273615ca2f3125eab
Wisaacj <wisaacj@live.co.uk>
2021-05-03 21:43:43 +0100

    Memory allocation issues generally fixed

--- Q3/CWK2Q3.c
+++ Q3/CWK2Q3.c
@@ -23,9 +23,9 @@
 #include <string.h>
 
 char** hash_map;  // This is where you should store your names
-int map_size;
-int no_items = 0; // Used to hold the number of items in the hash map
-float load_factor; // Used to automatically resize the map
+int map_size = 0; // Used to store the current max size of the hash table
+int no_items = 0; // Used to hold the number of items in the hash table
+float load_factor; // Used to automatically resize the table
 
 int hash_function(const char *key) {
 
@@ -45,33 +45,74 @@ int hash_function(const char *key) {
 
 void resize_map(int new_size) {
 
+    if (hash_map == NULL) {
+        hash_map = (char**) malloc(new_size*sizeof(char*)); // Allocating memory for the hash_map
+    } else {
+        hash_map = (char**) realloc(hash_map, (new_size*sizeof(char*))); // Reallocating the hash_maps's memory to be of the new size
+    }
+
+    if (hash_map == NULL) {
+        // Memory allocation failed, cleanup process
+        perror("Memory allocation failed, starting cleanup process...");
+        for (int i = 0; i < map_size; i++) {
+            free(hash_map[i]);
+        }
+        free(hash_map);
+        return;
+    }
+
+    // The allocation was successful
     // Resetting the global integer 'map_size' to the new size
     map_size = new_size;
-    // Reallocating the hash_maps's memory to be of the new size
-    hash_map = (char**) realloc(hash_map, sizeof(char) * new_size);
+
+}
+
+void assign_memory_for_bucket(int hash_value, int size_of_bucket) {
+
+    char* temp;
+
+    if (hash_map[hash_value] == 0x0) {
+        temp = (char*) malloc((size_of_bucket + 1) * sizeof(char));
+    } else {
+        temp = (char*) realloc(hash_map[hash_value], (size_of_bucket + 1) * sizeof(char));
+    }
+
+    if(temp == NULL) {
+        // Bucket memory allocation failed, cleanup process
+        perror("Bucket memory allocation failed, starting cleanup process...");
+        free(temp);
+    }
+
+    hash_map[hash_value] = temp;
 
 }
 
 // The parameter 'forward_check' allows for linear probing both 'forwards' and 'backwards' through the array
-void assign_new_value(int hash_value, const char *name, int forward_check) {
+void assign_new_value(int hash_value, const char *name, int forward_check, int original_hash_value) {
+
+    // Assigning memory for bucket
+    assign_memory_for_bucket(hash_value, strlen(name));
 
     // Checking for collisions
-    if (hash_map[hash_value] == NULL) {
+    if (hash_map[hash_value] == NULL || !strcmp(hash_map[hash_value], "")) {
 
-        hash_map[hash_value] = name; // Adding the string to the hash map at the index defined by hash_value
+        strcpy(hash_map[hash_value], name); // Adding the string to the hash map at the index defined by hash_value
         no_items++; // As an item was successfully added to a bucket, the global integer variable no_items is incremented by one
+        printf("Your key: %s has been successfully inserted into the hash table", name);
 
     } else {
 
         // Conditional statement to allow for linear probing both backwards and forwards
-        if (forward_check && ++hash_value < map_size) {
-            forward_check = 0;
-        } else if (!forward_check && --hash_value >= 0) {
-            forward_check = 1;
+        if (forward_check > 0) {
+            hash_value = (original_hash_value + forward_check) % map_size;
+            forward_check = -(forward_check + 1);
+        } else {
+            hash_value = (original_hash_value + forward_check) % map_size;
+            forward_check = -(forward_check - 1);
         }
 
         // Using recursion to implement the linear probing
-        return assign_new_value(hash_value, name, forward_check);
+        return assign_new_value(hash_value, name, forward_check, original_hash_value);
 
     }
 
@@ -84,7 +125,7 @@ void add_to_map(const char *name) {
 
     // Assigning a new value at the index given by the hash function
     // Using linear probing with an interval of 1 if there are collisions
-    assign_new_value(hash_value, name, 1);
+    assign_new_value(hash_value, name, 1, hash_value);
 
     // Calculating the loading factor
     load_factor = (float) no_items / (float) map_size;
@@ -107,6 +148,18 @@ int search_map(const char *name) {
 }
 
 void print_map() {
+
+    // Looping through all the elements in the hash map
+    for (int i = 0; i < map_size; i++) {
+
+        // Checking if the bucket at index i is empty or not
+        if (hash_map[i] != NULL) {
+
+            printf("Element in bucket %d is %s", i , hash_map[i]);
+
+        }
+
+    }
 
 }
 
