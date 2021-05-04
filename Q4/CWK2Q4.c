d17ce0f93ff9132d0cfa7b64a51458ca01257834
Wisaacj <wisaacj@live.co.uk>
2021-05-04 03:17:00 +0100

    Created print list function

--- Q4/CWK2Q4.c
+++ Q4/CWK2Q4.c
@@ -30,7 +30,7 @@ typedef struct linked_list_node {
 
 // Defining some global variables to hold the head and tail of the linked list
 linked_list_node *head;
-linked_list_node *tail;
+linked_list_node *tail; // May not need this variable
 
 struct linked_list_node *xor_node(char *name) {
     struct linked_list_node *new;
@@ -70,7 +70,6 @@ void insert_string(const char* newObj) {
             head->npx = calc_xor(new_node, calc_xor(0x0, head->npx));
             head = new_node;
         }
-
     } else {
         perror("Error: String object is too long.\n");
         perror("Strings should be less than 64 characters in length\n");
@@ -79,31 +78,92 @@ void insert_string(const char* newObj) {
 }
 
 
-int insert_before(const char* before, const char* newObj){
+int insert_before(const char* before, const char* newObj) {
+
+    linked_list_node *curr = head;
+    linked_list_node *prev = 0x0, *next;
+
+    while (strcmp(curr->name, before) != 0) {
+        next = calc_xor(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+
+    
 	
 }
 
 
 int insert_after(const char* after, const char* newObj) {
+
+    linked_list_node *curr = head;
+    linked_list_node *prev = 0x0, *next;
+
+    while (curr != 0x0) {
+        printf("Item: %s\n", curr->name);
+        next = calc_xor(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
 	
 }
 
 
 int remove_string(char* result){
+
+    linked_list_node *curr = head;
+    linked_list_node *prev = 0x0, *next;
+
+    while (curr != 0x0) {
+        printf("Item: %s\n", curr->name);
+        next = calc_xor(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
 	
 }
 
 
 int remove_after(const char *after, char *result){
+
+    linked_list_node *curr = head;
+    linked_list_node *prev = 0x0, *next;
+
+    while (curr != 0x0) {
+        printf("Item: %s\n", curr->name);
+        next = calc_xor(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
 	
 }
 
 
 int remove_before(const char *before, char *result) {
+
+    linked_list_node *curr = head;
+    linked_list_node *prev = 0x0, *next;
+
+    while (curr != 0x0) {
+        printf("Item: %s\n", curr->name);
+        next = calc_xor(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
 	
 }
 
-void print_list(){
+void print_list() {
+
+    linked_list_node *curr = head;
+    linked_list_node *prev = 0x0, *next;
+
+    while (curr != 0x0) {
+        printf("Item: %s\n", curr->name);
+        next = calc_xor(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
 	
 }
 
